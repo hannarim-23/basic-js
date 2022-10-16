a967b33782b763e158963179db4d04ccbd680d99
hannarim-23 <80169404+hannarim-23@users.noreply.github.com>
2022-10-16 05:27:26 +0300

    feat: Encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,8 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
+ function encodeLine(str) {
+  if (str == '') return '';
 
+  let newStr = [], count = 1;
+
+  for (let i=0; i < str.length; ){
+    while (str[i] == str[i+1]){
+      count++;
+      i++;
+    }
+    if (count > 1) newStr.push(`${count}${str[i]}`);
+    else newStr.push(`${str[i]}`)
+    i++;
+    
+    count =1;
+}
+  return newStr.join('');
 }
 
 module.exports = {
